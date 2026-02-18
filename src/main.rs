1d02c6fe0a15facab932ca5077c9a1f0194ea875
ADAMADA8 <adamk6344@gmail.com>
2026-02-19 02:37:32 +0400

    Update API endpoint to calculate sum of numbers

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use axum::{
-    routing::get,
-    Router,
-};
-use rand::{rng, RngExt};
+use axum::extract::RawQuery;
+use axum::http::StatusCode;
+use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app_routes = Router::new().route("/random", get(random));
-    let app = Router::new().nest("/api/v0", app_routes);
+    let app_routes = Router::new().route("/plus", get(plus));
+    let app = Router::new().nest("/api/v1", app_routes);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn random() -> String {
-    rng().random_range(1..100).to_string()
+async fn plus(RawQuery(query): RawQuery) -> Result<String, StatusCode> {
+    let query = query.unwrap_or_default();
+
+    let numbers: Result<Vec<i64>, _> = query.split(',').map(|s| s.parse::<i64>()).collect();
+
+    let nums = numbers.map_err(|_| StatusCode::BAD_REQUEST)?;
+    let sum: i64 = nums.iter().sum();
+    Ok(sum.to_string())
 }
