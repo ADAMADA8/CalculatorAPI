b45d5ec8d6095236dd48d0b053b7c05638c6674d
ADAMADA8 <adamk6344@gmail.com>
2026-02-20 03:32:39 +0400

    Rename plus and minus handlers to add and subtract, and add multiply and divide endpoints

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,16 +3,34 @@ use anyhow::Result;
 use axum::extract::RawQuery;
 use axum::http::StatusCode;
 
-pub(crate) async fn plus(RawQuery(query): RawQuery) -> Result<String, StatusCode> {
+pub(crate) async fn add(RawQuery(query): RawQuery) -> Result<String, StatusCode> {
     let result: i64 = parse_numbers(query)?.into_iter().sum();
 
     Ok(result.to_string())
 }
 
-pub(crate) async fn minus(RawQuery(query): RawQuery) -> Result<String, StatusCode> {
+pub(crate) async fn subtract(RawQuery(query): RawQuery) -> Result<String, StatusCode> {
     let result: i64 = parse_numbers(query)?
         .into_iter()
         .reduce(|acc, x| acc - x)
+        .ok_or(StatusCode::BAD_REQUEST)?;
+
+    Ok(result.to_string())
+}
+
+pub(crate) async fn multiply(RawQuery(query): RawQuery) -> Result<String, StatusCode> {
+    let result: i64 = parse_numbers(query)?
+        .into_iter()
+        .reduce(|acc, x| acc * x)
+        .ok_or(StatusCode::BAD_REQUEST)?;
+
+    Ok(result.to_string())
+}
+
+pub(crate) async fn divide(RawQuery(query): RawQuery) -> Result<String, StatusCode> {
+    let result: i64 = parse_numbers(query)?
+        .into_iter()
+        .reduce(|acc, x| acc / x)
         .ok_or(StatusCode::BAD_REQUEST)?;
 
     Ok(result.to_string())
--- src/services.rs
+++ src/services.rs
@@ -1,10 +1,12 @@
-use crate::handlers::{minus, plus};
+use crate::handlers::{subtract, add, multiply, divide};
 use axum::Router;
 use axum::routing::get;
 
 pub(crate) fn app_router() -> Router {
     let app_routes = Router::new()
-        .route("/plus", get(plus))
-        .route("/minus", get(minus));
+        .route("/add", get(add))
+        .route("/subtract", get(subtract))
+        .route("/multiply", get(multiply))
+        .route("/divide", get(divide));
     Router::new().nest("/api/v1", app_routes)
 }
