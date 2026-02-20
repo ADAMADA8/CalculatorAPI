3403afffe8788e388a821000442e56e9b91a111c
ADAMADA8 <adamk6344@gmail.com>
2026-02-20 04:30:35 +0400

    Improve add and bug fix for subtract

--- src/handlers.rs
+++ src/handlers.rs
@@ -4,16 +4,22 @@ use axum::extract::RawQuery;
 use axum::http::StatusCode;
 
 pub(crate) async fn add(RawQuery(query): RawQuery) -> Result<String, StatusCode> {
-    let result: i64 = parse_numbers(query)?
-        .try_fold(0i64, |acc, x| acc.checked_add(x))
+    let mut numbers = parse_numbers(query)?;
+    let first = numbers.next().ok_or(StatusCode::UNPROCESSABLE_ENTITY)?;
+
+    let result: i64 = numbers
+        .try_fold(first, |acc, x| acc.checked_add(x))
         .ok_or(StatusCode::UNPROCESSABLE_ENTITY)?;
 
     Ok(result.to_string())
 }
 
 pub(crate) async fn subtract(RawQuery(query): RawQuery) -> Result<String, StatusCode> {
-    let result: i64 = parse_numbers(query)?
-        .try_fold(0i64, |acc, x| acc.checked_sub(x))
+    let mut numbers = parse_numbers(query)?;
+    let first = numbers.next().ok_or(StatusCode::UNPROCESSABLE_ENTITY)?;
+
+    let result: i64 = numbers
+        .try_fold(first, |acc, x| acc.checked_sub(x))
         .ok_or(StatusCode::UNPROCESSABLE_ENTITY)?;
 
     Ok(result.to_string())
